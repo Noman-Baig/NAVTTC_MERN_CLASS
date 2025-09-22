d8016fa5bc99655077e8d2675ad01fe5207f1e34
Mirza Noman Baig <98812536+Noman-Baig@users.noreply.github.com>
2025-09-22 14:11:15 +0500

    Create if_else_class.js

---
+++ if_else_class.js
@@ -0,0 +1,25 @@
+if // condiiton
+else if // condiiton
+else if // both condition not worked then this works
+
+let age = 20;
+
+if (age < 13) {
+  console.log("Child");
+} else if (age < 18) {
+  console.log("Teenager");
+} else if (age < 60) {
+  console.log("Adult");
+} else {
+  console.log("Senior Citizen");
+}
+
+
+console.log(a + b); 
+console.log(a - b);   
+console.log(a * b);  
+console.log(a / b);  
+console.log(a % b);   
+console.log(a ** b);  
+console.log(a++);      
+console.log(b--);  
